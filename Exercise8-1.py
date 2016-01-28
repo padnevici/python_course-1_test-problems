b8b32b00100bad7ce0e9412fb8e71a6551f3dfd3
Andrei <padnevici@gmail.com>
2016-01-28 00:19:33 -0500

    * Added checkers in functions at Exercise8-1.py. Now list with 0 elements will not produce traceback

--- Exercise8-1.py
+++ Exercise8-1.py
@@ -7,21 +7,26 @@ Created on Jan 24, 2016
 import random
 
 
-def chop(list):
-    del list[0]
-    del list[len(list) - 1]
+def chop(listVals):
+    if len(listVals) > 2:
+        del listVals[0]
+        del listVals[len(listVals) - 1]
 
 
-def middle(list):
-    return list[1:len(list) - 1]
+def middle(listVal):
+    if len(listVal) > 2:
+        return listVal[1:len(listVal) - 1]
+    else:
+        return listVal
 
 
-list = list(range(random.randrange(10, 20)))
+for x in range(10):
+    listVals = list(range(random.randrange(0, 10)))
 
-print(list)
-chop(list)
-print(list)
+    print(listVals)
+    chop(listVals)
+    print(listVals)
 
-newList = middle(list)
-print(list)
-print(newList)
+    newList = middle(listVals)
+    print(listVals)
+    print(newList)
