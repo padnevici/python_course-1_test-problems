c5ae62575e04989e22996a38402c3b499d2215c4
Andrei Padnevici <padnevici@gmail.com>
2016-02-23 16:32:54 -0500

    * added inheritance to the OOP example

--- Objects.py
+++ Objects.py
@@ -10,13 +10,35 @@ from tkinter import Pack
 class PartyAnimal:
     x = 0
 
+    def __init__(self, x=-1):
+        self.x = x
+
     def party(self):
-        self.x+=1
+        self.x += 1
         print("So far", self.x)
+
+
+class ChildParty(PartyAnimal):
+    y = 0
+
+    def __init__(self, x=0, y=0):
+        super(ChildParty, self).__init__(x)
+        self.y = y
+
+    def sum(self):
+        print(self.x + self.y)
+
 
 p = PartyAnimal()
 p.party()
 p.party()
 p.party()
 
-PartyAnimal.party(PartyAnimal())
+p = PartyAnimal(5)
+p.party()
+p.party()
+p.party()
+
+c = ChildParty(9, 9)
+c.party()
+c.sum()
